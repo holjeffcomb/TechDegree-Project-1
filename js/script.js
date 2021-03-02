6a214237ee1b6d9ee82911feec5ff8ce7a93185c
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-03-02 12:13:51 -0800

    added more functionality
    added more quotes, added setInterval function, made background color randomized

--- js/script.js
+++ js/script.js
@@ -11,18 +11,17 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let main = document.querySelector('main');
+
 let quotes = [
-  {
-    author: 'Bob Marley',
-    quote: 'Some people feel the rain, others just get wet.'
-  },
   {
     author: 'Stephen Hawking',
     quote: 'Quiet people have the loudest minds.'
   },
   {
     author: 'Martin Luther King Jr.',
-    quote: 'Let no man pull you low enough to hate him.'
+    quote: 'Let no man pull you low enough to hate him.',
+    year: '1956'
   },
   {
     author: 'Maya Angelou',
@@ -38,7 +37,8 @@ let quotes = [
   },
   {
     author: 'The Dalai Lama',
-    quote: 'My religion is kindness.'
+    quote: 'My religion is kindness.',
+    tag: 'spirituality'
   },
   {
     author: 'George Washington',
@@ -50,7 +50,10 @@ let quotes = [
   },
   {
     author: 'Ram Dass',
-    quote: 'We\'re all just walking eachother home.'
+    quote: 'We\'re all just walking eachother home.',
+    citation: 'Be Here Now',
+    year: '1971',
+    tag: 'spirituality'
   },
   {
     author: 'Bill Nye',
@@ -62,39 +65,48 @@ let quotes = [
   },
   {
     author: 'Bob Marley',
-    quote: 'Emancipate yourselves from mental slavery. None but ourselves can free our minds.'
-  },
-  {
-    author: 'Malcolm X',
-    quote: 'A man who stands for nothing will fall for anything.'
+    quote: 'Emancipate yourselves from mental slavery. None but ourselves can free our minds.',
+    citation: 'Redemption Song',
+    year: '1980',
+    tag: 'spirituality'
   },
   {
     author: 'Dr. Seuss',
     quote: 'Today I shall behave as if this is the day I will be remembered.'
   },
   {
+    author: 'Aristotle',
+    quote: 'It is during our darkest moments that we must focus to see the light.'
+  },
+  {
     author: 'Dr. Seuss',
-    quote: 'From there to here and here to there, funny things are everywhere.'
+    quote: 'From there to here and here to there, funny things are everywhere.',
+    tag: 'humor'
   },
   {
     author: 'David Goggins',
-    quote: 'I don\'t stop when I\'m tired, I stop when I\'m done.'
+    quote: 'I don\'t stop when I\'m tired, I stop when I\'m done.',
+    tag: 'inspiration'
   },
   {
     author: 'David Goggins',
-    quote: 'Denial is the ultimate comfort zone.'
+    quote: 'Denial is the ultimate comfort zone.',
+    tag: 'inspiration'
   },
   {
     author: 'Michael Jordan',
-    quote: 'You have to expect things of yourself before you can do them.'
+    quote: 'You have to expect things of yourself before you can do them.',
+    tag: 'inspiration'
   },
   {
     author: 'Michael Jordan',
-    quote: 'You miss 100% of the shots you don\'t take.'
+    quote: 'You miss 100% of the shots you don\'t take.',
+    tag: 'inspiration'
   },
   {
     author: 'Rumi',
-    quote: 'Don\'t be satisfied with stories, how things have gone with others. Unfold your own myth.'
+    quote: 'Don\'t be satisfied with stories, how things have gone with others. Unfold your own myth.',
+    tag: 'inspiration'
   },
   {
     author: 'Rumi', 
@@ -102,27 +114,48 @@ let quotes = [
   },
   {
     author: 'Eminem',
-    quote: 'A normal life is boring.'
-  },
-  {
-    author: 'Eminem',
-    quote: 'Some people dream of success while others wake up and work hard at it.'
+    quote: 'A normal life is boring.',
+    citation: 'Lose Yourself',
+    year: '2002'
   },
   {
     author: 'Shigeru Miyamoto',
-    quote: 'A delayed game is eventually good, but a rushed game is always bad.'
+    quote: 'A delayed game is eventually good, but a rushed game is always bad.',
+    year: '1996'
   },
   {
     author: 'JRR Tolkien',
-    quote: 'Not all those who wander are lost.'
+    quote: 'Not all those who wander are lost.',
+    citation: 'The Fellowship of the Ring',
+    year: '1954'
   },
   {
     author: 'JRR Tolkien',
-    quote: 'Even the smallest person can change the course of the future.'
+    quote: 'Even the smallest person can change the course of the future.',
+    citation: 'The Fellowship of the Ring',
+    year: '1954'
   },
   {
     author: 'The Rock',
     quote: 'Be humble. Be hungry. And always be the hardest worker in the room.'
+  },
+  {
+    author: 'Paul Coelho',
+    quote: 'And, when you want something, all the universe conspires in helping you to achieve it.',
+    citation: 'The Alchemist',
+    year: '1988'
+  },
+  {
+    author: 'Dan Millman',
+    quote: 'There are no oridinary moments.',
+    citation: 'Way of the Peaceful Warrior',
+    year: '1980'
+  },
+  {
+    author: 'Lao Tzu',
+    quote: 'Those who know do not speak. Those who speak do not know.',
+    citation: 'Tao Te Ching',
+    year: '4th century BC'
   }
 ];
 
@@ -130,14 +163,60 @@ let quotes = [
  * `getRandomQuote` function
 ***/
 
-function getRandomQuote (arr, arrLength) {
-  Math.floor(Math.random() * arrLength);
-  console.log();
+function getRandomQuote (arrLength) {
+  let index = Math.floor(Math.random() * arrLength);
+  return quotes[index];
 }
+
+
+
 
 /***
  * `printQuote` function
 ***/
+
+function printQuote() {
+
+  // Random hexadecimal color generator, copied from https://css-tricks.com/snippets/javascript/random-hex-color/
+  const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+  }
+  setBg();
+
+  let quote = getRandomQuote(quotes.length);
+
+  let html = '';
+
+  html = `
+    <div class="container">
+      <div id="quote-box" class="quote-box">
+        <p class="quote">${quote.quote}</p>
+        <p class="source">${quote.author}
+  `;
+
+  if (quote.citation){
+    html += `<span class="citation">${quote.citation}</span>`;
+  }
+
+  if (quote.year){
+    html += `<span class="year">${quote.year}</span>`;
+  }
+
+  html += `             
+        </p>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = html;
+
+}
+
+
+
+setInterval(printQuote, 10000);
+printQuote();
 
 
 
