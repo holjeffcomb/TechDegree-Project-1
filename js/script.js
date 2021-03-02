23025200c5326e08eb2d05c6fe41401a1b7adc9f
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-03-02 13:51:46 -0800

    changed author to source

--- js/script.js
+++ js/script.js
@@ -15,146 +15,146 @@ project 1 - A Random Quote Generator
 
 let quotes = [
   {
-    author: 'Stephen Hawking',
+    source: 'Stephen Hawking',
     quote: 'Quiet people have the loudest minds.'
   },
   {
-    author: 'Martin Luther King Jr.',
+    source: 'Martin Luther King Jr.',
     quote: 'Let no man pull you low enough to hate him.',
     year: '1956'
   },
   {
-    author: 'Maya Angelou',
+    source: 'Maya Angelou',
     quote: 'If you\'re always trying to be normal you will never know how amazing you can be.'
   },
   {
-    author: 'Maya Angelou',
+    source: 'Maya Angelou',
     quote: 'You may kill me with your hatefulness, but still, like air, I\'ll rise.'
   },
   {
-    author: 'The Dalai Lama',
+    source: 'The Dalai Lama',
     quote: 'Compassion is the radicalism of our time.'
   },
   {
-    author: 'The Dalai Lama',
+    source: 'The Dalai Lama',
     quote: 'My religion is kindness.',
     tag: 'spirituality'
   },
   {
-    author: 'George Washington',
+    source: 'George Washington',
     quote: 'The harder the conflict, the greater the triumph'
   },
   {
-    author: 'Albert Einstein',
+    source: 'Albert Einstein',
     quote: 'I have no special talents. I am only passionately curious.'
   },
   {
-    author: 'Ram Dass',
+    source: 'Ram Dass',
     quote: 'We\'re all just walking eachother home.',
     citation: 'Be Here Now',
     year: '1971',
     tag: 'spirituality'
   },
   {
-    author: 'Bill Nye',
+    source: 'Bill Nye',
     quote: 'Everyone you meet knows something you don\'t.'
   },
   {
-    author: 'Martin Luther King Jr.',
+    source: 'Martin Luther King Jr.',
     quote: 'Out of the mountain of despair, a stone of hope.'
   },
   {
-    author: 'Bob Marley',
+    source: 'Bob Marley',
     quote: 'Emancipate yourselves from mental slavery. None but ourselves can free our minds.',
     citation: 'Redemption Song',
     year: '1980',
     tag: 'spirituality'
   },
   {
-    author: 'Dr. Seuss',
+    source: 'Dr. Seuss',
     quote: 'Today I shall behave as if this is the day I will be remembered.'
   },
   {
-    author: 'Aristotle',
+    source: 'Aristotle',
     quote: 'It is during our darkest moments that we must focus to see the light.'
   },
   {
-    author: 'Dr. Seuss',
+    source: 'Dr. Seuss',
     quote: 'From there to here and here to there, funny things are everywhere.',
     tag: 'humor'
   },
   {
-    author: 'David Goggins',
+    source: 'David Goggins',
     quote: 'I don\'t stop when I\'m tired, I stop when I\'m done.',
     tag: 'inspiration'
   },
   {
-    author: 'David Goggins',
+    source: 'David Goggins',
     quote: 'Denial is the ultimate comfort zone.',
     tag: 'inspiration'
   },
   {
-    author: 'Michael Jordan',
+    source: 'Michael Jordan',
     quote: 'You have to expect things of yourself before you can do them.',
     tag: 'inspiration'
   },
   {
-    author: 'Michael Jordan',
+    source: 'Michael Jordan',
     quote: 'You miss 100% of the shots you don\'t take.',
     tag: 'inspiration'
   },
   {
-    author: 'Rumi',
+    source: 'Rumi',
     quote: 'Don\'t be satisfied with stories, how things have gone with others. Unfold your own myth.',
     tag: 'inspiration'
   },
   {
-    author: 'Rumi', 
+    source: 'Rumi', 
     quote: 'Stop acting so small. You are the universe in ecstatic motion.',
     tag: 'inspiration'
   },
   {
-    author: 'Eminem',
+    source: 'Eminem',
     quote: 'A normal life is boring.',
     citation: 'Lose Yourself',
     year: '2002'
   },
   {
-    author: 'Shigeru Miyamoto',
+    source: 'Shigeru Miyamoto',
     quote: 'A delayed game is eventually good, but a rushed game is always bad.',
     year: '1996'
   },
   {
-    author: 'JRR Tolkien',
+    source: 'JRR Tolkien',
     quote: 'Not all those who wander are lost.',
     citation: 'The Fellowship of the Ring',
     year: '1954'
   },
   {
-    author: 'JRR Tolkien',
+    source: 'JRR Tolkien',
     quote: 'Even the smallest person can change the course of the future.',
     citation: 'The Fellowship of the Ring',
     year: '1954'
   },
   {
-    author: 'The Rock',
+    source: 'The Rock',
     quote: 'Be humble. Be hungry. And always be the hardest worker in the room.'
   },
   {
-    author: 'Paul Coelho',
+    source: 'Paul Coelho',
     quote: 'And, when you want something, all the universe conspires in helping you to achieve it.',
     citation: 'The Alchemist',
     year: '1988'
   },
   {
-    author: 'Dan Millman',
+    source: 'Dan Millman',
     quote: 'There are no oridinary moments.',
     citation: 'Way of the Peaceful Warrior',
     year: '1980',
     tag: 'spirituality'
   },
   {
-    author: 'Lao Tzu',
+    source: 'Lao Tzu',
     quote: 'Those who know do not speak. Those who speak do not know.',
     citation: 'Tao Te Ching',
     year: '4th century BC',
@@ -196,7 +196,7 @@ function printQuote() {
     <div class="container">
       <div id="quote-box" class="quote-box">
         <p class="quote">${quote.quote}</p>
-        <p class="source">${quote.author}`;
+        <p class="source">${quote.source}`;
 
   if (quote.citation){
     html += `<span class="citation">${quote.citation}</span>`;
